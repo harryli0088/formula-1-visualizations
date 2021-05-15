b2aac5122a4a76a2c5a59a9f6b41fde4ae95885e
harryli0088 <harryli0088@gmail.com>
2021-05-15 16:19:09 -0400

    improvements

--- client/src/Components/Popover.svelte
+++ client/src/Components/Popover.svelte
@@ -1,60 +1,66 @@
 <script lang="ts">
   export let content: string = ""
+  export let fullWidth: number = 400 //the full width of the popover if the screen is wide enough
 
+  let innerWidth: number
   let popoverContainer 
   let show: boolean = true
 
-  $: bounds = popoverContainer ? popoverContainer.getBoundingClientRect() : {
-    bottom: 0,
-    height: 0,
-    left: 0,
-    right: 0,
-    top: 0,
-    width: 0,
-    x: 0,
-    y: 0,
-  }
-  $: console.log(bounds)
+  
+
+
+
+
+
+
+  /**
+   * Calculate the CSS positions for the popover
+   * @param bounds      //output from getBoundingClientRect
+   * @param fullWidth   //desired full width, if the screen is big enough
+   * @param innerWidth  //current inner width of the screen
+   */
   function getPositions(
     bounds: ClientRect,
-    innerWidth: number,
+    fullWidth: number,
+    innerWidth: number = 0,
   ) {
-    const defaultWidth = 500
-    let top = -15
-    const popoverWidth = bounds.right - bounds.left
-    let fixedCenter = (bounds.right + bounds.left) / 2
-    let fixedLeft = (fixedCenter - defaultWidth / 2)
-    let fixedRight = (fixedCenter + defaultWidth / 2)
-    let left = popoverWidth/2 - defaultWidth/2
-    let right = popoverWidth/2 - defaultWidth/2
+    const fixedCenter = (bounds.right + bounds.left) / 2 //center of popover container, relative to the page, ie css position: fixed
+    const fixedLeft = (fixedCenter - fullWidth / 2) //left position of popover container
+    const fixedRight = (fixedCenter + fullWidth / 2) //right position of popover container
+    const popoverContainerWidth = bounds.right - bounds.left //width of the popover container
 
-    let leftOffset = Math.max(0, -fixedLeft)
-    let rightOffset = Math.max(0, fixedRight - innerWidth)
+    //calculate the initial left and right values
+    const startingValue = popoverContainerWidth/2 - fullWidth/2
+    let left = startingValue
+    let right = startingValue
 
-    left += leftOffset
-    right += rightOffset
-    if(innerWidth > defaultWidth) {
+    let leftOffset = Math.max(0, -fixedLeft) //determine the left offset if the popover extends too far left off the page
+    let rightOffset = Math.max(0, fixedRight - innerWidth) //determine the right offset if the popover extends too far right off the page
+
+    left += leftOffset //move the left value by the offset
+    right += rightOffset //move the left value by the offset
+    if(innerWidth > fullWidth) { //if the page is big enough for the full width
+      //move the left and right values accordingly
       left -= rightOffset
       right -= leftOffset
     }
     
-
-    return {
-      top,
-      left,
-      right
-    }
+    return { top: -15, left, right }
   }
 
+  $: bounds = (
+    innerWidth && popoverContainer //if the popover is mounted
+    ? popoverContainer.getBoundingClientRect() //get the bounding client rectangle
+    : { bottom: 0, height: 0, left: 0, right: 0, top: 0, width: 0, x: 0, y: 0 } //else fake the client rect with zero values
+  )
+
   $: (
-    {
-      top,
-      left,
-      right
-    } = getPositions(bounds, window.innerWidth)
+    { top, left, right } = getPositions(bounds, fullWidth, innerWidth)
   )
 
 </script>
+
+<svelte:window bind:innerWidth={innerWidth}/>
 
 <span
   bind:this={popoverContainer}
